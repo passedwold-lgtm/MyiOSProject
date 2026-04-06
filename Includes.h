37b71f35be3286466c3ac0b35c4f7d89fb944cb6
passedwold-lgtm <passedwold@gmail.com>
2026-04-06 18:26:28 +0700

    Update Includes.h

--- Includes.h
+++ Includes.h
@@ -3,23 +3,26 @@
 #include <iostream>
 #include <UIKit/UIKit.h>
 #include <vector>
-#import "pthread.h"
+#import <pthread.h>
 #include <array>
 #import <os/log.h>
-#import "pthread.h"
 #include <cmath>
 #include <deque>
-#include <vector>
 #include <fstream>
 #include <algorithm>
+
 #define UIColorFromHex(hexColor) [UIColor colorWithRed:((float)((hexColor & 0xFF0000) >> 16))/255.0 green:((float)((hexColor & 0xFF00) >> 8))/255.0 blue:((float)(hexColor & 0xFF))/255.0 alpha:1.0]
 
 #import "ImGuiDrawView.h"
 #import "LoadView.h"
 #import "FTNotificationIndicator.h"
-#import "../imgui/imgui.h"
-#import "../imgui/imgui_internal.h"
-#import "../imgui/imgui_impl_metal.h"
+
+/// 🔥 FIX IMGUi PATH
+#import "imgui.h"
+#import "imgui_internal.h"
+#import "imgui_impl_metal.h"
+
+/// 🔥 FIX OBFUSCATE PATH
 #include "Security/Obfuscate.h"
 
 #define kWidth  [UIScreen mainScreen].bounds.size.width
